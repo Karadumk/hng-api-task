7cb43164282b8f78bb055762f99ec0863c34dce6
Karadumk <karadumk223@gmail.com>
2025-02-03 16:11:57 +0100

    correct datetime format

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ def get_info():
     response = {
         "email": "Kgkpt2018@gmail.com",
         # figure out UTC datetime last one wasnt correct
-        "current_datetime": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "github_url": "https://github.com/Karadumk/hng-api-task"
+        "current_datetime": datetime.now(timezone.utc)
+        .replace(microsecond=0)
+        .strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "github_url": "https://github.com/Karadumk/hng-api-task",
     }
     return jsonify(response), 200
+
 
 if __name__ == "__main__":
     # app.run(debug=True)
     # Set host to "0.0.0.0" so the API is accessible externally if deployed
     app.run(host="0.0.0.0", port=5000, debug=True)
-   
